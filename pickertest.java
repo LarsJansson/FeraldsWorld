46907c94b8f67193e96f2239b6bfe3ab28f01e2e
Felix Viberg <felixwiberg@gmail.com>
2018-04-03 03:53:04 +0200

    blandat

---
+++ pickertest.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+import java.awt.event.*;
+
+public class pickertest{
+    int key;
+    int dx;
+    public void run(){
+        System.out.print("^");
+        Scanner m = new Scanner(Console.in);
+        while(true){
+            String key = m.nextLine();
+            if(key != null){
+                System.out.println(key);
+            }
+        }
+    }
+}
---
+++ pickertestmain.java
@@ -0,0 +1,11 @@
+public class pickertestmain{
+    public static void main(String[] args){
+        pickertest m = new pickertest();
+        m.run();
+    }
+}
+
+
+
+
+
