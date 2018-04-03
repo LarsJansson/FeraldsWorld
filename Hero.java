6938e5d988bbbcf7e8fdecdb445833da52a5866c
Felix <felixwiberg@gmail.com>
2018-04-03 21:55:00 +0200

    colors for hero classes

--- Hero.java
+++ Hero.java
@@ -1,5 +1,15 @@
 public class Hero{
-    private String gender = "";
+private String gender = "";
+
+    public static final String RESET    = "\u001B[0m";
+    public static final String RED      = "\u001B[31m";
+    public static final String GREEN    = "\u001B[32m";
+    public static final String YELLOW   = "\u001B[33m";
+    public static final String BLUE     = "\u001B[34m";
+    public static final String PURPLE   = "\u001B[35m";
+    public static final String CYAN     = "\u001B[36m";
+    public static final String WHITE    = "\u001B[37m";
+
     private String race = "";
     private String name = "";
 
@@ -15,6 +25,24 @@ public class Hero{
         this.gender = gender;
     }
 
+    public String getName(int c){
+        return colorize(this.name, c);
+    }
+    public String getRace(int c){
+        return colorize(this.race, c);
+    }
+    public String getGender(int c){
+        return colorize(this.gender, c);
+    }
+    public String getNameU(int c){
+        return colorize(this.name.toUpperCase(), c);
+    }
+    public String getRaceU(int c){
+        return colorize(this.race.toUpperCase(), c);
+    }
+    public String getGenderU(int c){
+        return colorize(this.gender.toUpperCase(), c);
+    }
     public boolean isHuman(){
         if (race.equals("Human"))
             return true;
@@ -50,5 +78,24 @@ public class Hero{
             return true;
         else
             return false;
+    }
+    public String colorize(String s, int c){
+        String pickedColor = "";
+        if (c == 1)
+            return RED+s+RESET;
+        else if (c == 1)
+            return GREEN+s+RESET;
+        else if (c == 2)
+            return YELLOW+s+RESET;
+        else if (c == 3)
+            return BLUE+s+RESET;
+        else if (c == 4)
+            return PURPLE+s+RESET;
+        else if (c == 5)
+            return CYAN+s+RESET;
+        else if (c == 6)
+            return WHITE+s+RESET;
+        else
+            return s;
     }
 }
