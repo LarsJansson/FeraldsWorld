5df658a7568bc6daa2d2491c5f143abeea94fa67
Felix <felixwiberg@gmail.com>
2018-04-03 15:56:11 +0200

    bugfix

--- output.java
+++ output.java
@@ -1,13 +1,15 @@
+import java.util.concurrent.TimeUnit;
+
 public class output{
     public output(){
         System.out.println("Output> No string provided");
     }
-    public output(String str){
+    public output(String str) throws Exception{
         System.out.println(str);
         TimeUnit.SECONDS.sleep(0);
     }
-    public output(String str, int delay){
+    public output(String str, int delay) throws Exception{
         System.out.println(str);
-        Timunit.SECONDS.sleep(delay);
+        TimeUnit.SECONDS.sleep(delay);
     }
 }
