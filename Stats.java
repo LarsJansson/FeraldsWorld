55f8f57373df20c9302fc56eb17cc04e5e4bd0e2
Felix Viberg <felixwiberg@gmail.com>
2018-04-06 15:50:21 +0200

    adding stats class

---
+++ Stats.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.TimeUnit;
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+
+public class Stats{
+
+    public void Stats(JTextPane tp){
+        tp.setText("STATS");
+    }
+
+    public void clear(JTextPane tp){
+        tp.setText("STATS");
+    }
+
+    public void updateStats(JTextPane tp, Hero hero) throws Exception{
+        System.out.println(hero);
+        StyledDocument doc = tp.getStyledDocument();
+        AttributeSet set = doc.getCharacterElement(1).getAttributes();
+        Style style = tp.addStyle("", null);
+        StyleConstants.setForeground(style, Color.WHITE);
+        tp.setCharacterAttributes(style, true);
+        
+        try {
+            doc.insertString(doc.getLength(), "\n", style);
+            doc.insertString(doc.getLength(),
+                    "Name: " + hero.getName() + "\t" + "Race: " + hero.getRace() + "\t" +  "Gender: " + hero.getGender(), style);
+            }
+        catch (BadLocationException e){}
+    }
+}
