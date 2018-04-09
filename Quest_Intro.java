ba044d64add3a77d1865864e598b190d22298eb9
Ferald <larsjansson88@gmail.com>
2018-04-09 20:48:00 +0200

    d

--- Quest_Intro.java
+++ Quest_Intro.java
@@ -33,130 +33,52 @@ public class Quest_Intro extends TheGame{
                 o.output(model, t1, "Oooor...", 0.0, true);
                 o.output(model, t1, "Dwarf?", 0.0, true);
                 
-                inputPanel.setText("");
-                model.setInput("");
-                input = model.getInput();
-                
-                while(!model.getInput().equals("1") && !model.getInput().equals("2") && !model.getInput().equals("3") && !model.getInput().equals("4")){
-                    o.output(model,t1, "Since you're playing on a computer, choose a RACE by\nentering \"1\" (Human), \"2\" (Orch), \"3\" (Elf) or \"4\" (Dwarf):", 0.0, true, false);
-                    
+                racePick(model, t1, hero);
+
+                if(hero.isHuman()){
+                    o.output(model, t1, "Oh... A fancy human again. Oh well, to choose gender,\nenter a number between 1 and 10:", 2000*speed, true);
                     inputPanel.setText("");
                     model.setInput("");
                     input = model.getInput();
+                    
                     while(model.getInput().equals(""))
                         Thread.sleep(100);
-                    o.clearGUI(t1);
-                    if(model.getInput().equals("1")){
-                        hero.setRace("Human");
-                        o.output(model, t1, "Oh... A fancy human again. Oh well, to choose gender,\nenter a number between 1 and 10:", 2000*speed, true);
-                        inputPanel.setText("");
-                        model.setInput("");
-                        input = model.getInput();
-                    
-                        while(model.getInput().equals(""))
-                            Thread.sleep(100);
 
-                        if(!model.getInput().equals("")){
-                            o.output(model, t1, "Ok, this won't do...", 2000*speed, true);
-                            o.output(model, t1, "Please choose either male (\"1\"), female (\"2\") or transgender (\"3\")!\n", 0.0, true);
-                            
-                            inputPanel.setText("");
-                            model.setInput("");
-                            input = model.getInput();
-                        
-                            while(model.getInput().equals(""))
-                                Thread.sleep(100);
-                        
-                            if(model.getInput().equals("1"))
-                                hero.setGender("Male");
-                            else if(model.getInput().equals("2"))
-                                hero.setGender("Female");
-                            else
-                                hero.setGender("Transgender");
-                        }
-                    }
-                    else if(model.getInput().equals("2")){
-                        hero.setRace("Orch");
-                        o.output(model, t1, "A filthy orch? Really? Oh well, it's your choice!", 2000*speed, true);
-                        o.output(model, t1, "Now, choose your gender. Enter (\"1\") for male,(\"2\") for female or (\"3\") for transgender.", 0.0, true);
-                        inputPanel.setText("");model.setInput("");
-                        input = model.getInput();
- 
-                        while(model.getInput().equals(""))
-                            Thread.sleep(100);
-                        
-                        if(model.getInput().equals("1"))
-                            hero.setGender("Male");
-                        else if(model.getInput().equals("2"))
-                            hero.setGender("Female");
-                        else
-                            hero.setGender("Transgender");
-                    }
-                    else if(model.getInput().equals("3")){
-                        hero.setRace("Elf");
-                        o.output(model, t1, "Excellent choice! Elfs are famous for their wisdom!", 2000*speed, true);
-                        o.output(model, t1, "\nAnd their ears...", 0.0, false);
-                        o.output(model, t1, "Now, pick your gender! Enter (\"1\") for male,(\"2\") for female or (\"3\") for transgender.", 0.0, true);
-                        inputPanel.setText("");
-                        model.setInput("");
-                        input = model.getInput();
- 
-                        while(model.getInput().equals(""))
-                            Thread.sleep(100);
-                        
-                        if(model.getInput().equals("1"))
-                            hero.setGender("Male");
-                        else if(model.getInput().equals("2"))
-                            hero.setGender("Female");
-                        else
-                            hero.setGender("Transgender");
-                    }
-                    else if(model.getInput().equals("4")){
-                        hero.setRace("Dwarf");
-                        o.output(model, t1, "Oh hello! Didn't see you there! A true dwarf it is!", 2000*speed, true);
-                        o.output(model, t1, "Maybe this is a stupid question...", 0.0, true);
-                        o.output(model, t1, "\nDo dwarfs have genders?", 0.0, false);
-                        o.output(model, t1, "\nIf you do, enter (\"1\") for male,(\"2\") for female or (\"3\") for transgender.", 0.0, false);
-                        inputPanel.setText("");
-                        model.setInput("");
-                        input = model.getInput();
- 
-                        while(model.getInput().equals(""))
-                            Thread.sleep(100);
-                        
-                        if(model.getInput().equals("1"))
-                            hero.setGender("Male");
-                        else if(model.getInput().equals("2"))
-                            hero.setGender("Female");
-                        else
-                            hero.setGender("Transgender");                  
-                    }
-                    else{
-                        o.output(model, t1, "That was not an option!", 0.0, true);
-                    }
+                    o.output(model, t1, "Ok, this won't do...", 2000*speed, true);
+                    genderPick(model, t1, hero);
                 }
-                
+                else if(hero.isOrch()){
+                    o.output(model, t1, "A filthy orch? Really? Oh well, it's your choice!", 2000*speed, true);
+                    genderPick(model, t1, hero);
+                }
+                else if(hero.isElf()){
+                    o.output(model, t1, "Excellent choice! Elfs are famous for their wisdom!", 2000*speed, true);
+                    o.output(model, t1, "\nAnd their ears...", 0.0, false);
+                    genderPick(model, t1, hero);
+                }
+                else if(hero.isDwarf()){
+                    o.output(model, t1, "Oh hello! Didn't see you there! A true dwarf it is!", 2000*speed, true);
+                    o.output(model, t1, "Maybe this is a stupid question...", 0.0, true);
+                    o.output(model, t1, "\nDo dwarfs have genders?", 0.0, false);
+                    genderPick(model, t1, hero);
+                }
+                else
+                    o.output(model, t1, "That was not an option!", 0.0, true);
+
                 o.output(model, t1, "Ok. Now the game can finally begin!", 2000*speed, true);
                 o.output(model, t1, "Wait!", 0.0, true);
-                o.output(model, t1, "Whats your name?", 0.0, true);
-                        
-                inputPanel.setText("");
-                model.setInput("");
-                input = model.getInput();
- 
-                while(model.getInput().equals(""))
-                    Thread.sleep(100);
-                
-                hero.setName(model.getInput());
+                o.output(model, t1, "Whats your name?", 0.0, true);   
+                namePick(model, t1, hero);
 
-                o.output(model, t1, String.format("You have choosen to play as %s, the %s %s",hero.getNameU(),hero.getGenderU(),hero.getRaceU()), 2000*speed, true);
-                stats.updateStats(t3,hero);
+                o.output(model, t1, String.format("You have choosen to play as %s, the %s %s", hero.getNameU(), hero.getGenderU(), hero.getRaceU()), 2000*speed, true);
+                
+                stats.updateStats(t3, hero);
+
                 o.outputSlowQuote(model, t1, "Zzzz......", 0.0, 150*speed, true);
                 o.outputSlowQuote(model, t1, "Zzzzzzzz...", 0.0, 150*speed, true);
                 o.outputSlowQuote(model, t1, "Hrmph...", 0.0, 30*speed, true);
                 
                 String g;
-                
                 if(hero.getGender() == "Male")
                     g = "He";
                 else if(hero.getGender() == "Female")
@@ -164,7 +86,7 @@ public class Quest_Intro extends TheGame{
                 else
                     g = "He/she";
                
-                o.output(model, t1, g + " is waking up.", 0.0, true);
+                o.output(model, t1, g+" is waking up.", 0.0, true);
                 o.outputSlowQuote(model, t1, "What?", 0.0, 100*speed, true);
                 o.outputSlowQuote(model, t1, " Where am I?", 0.0, 100*speed, false);
                 o.output(model, t1, "As I tried to rise, it felt like the cold hard ground rose up around my body.", 0.0, true);
@@ -187,4 +109,54 @@ public class Quest_Intro extends TheGame{
         super.inv = inv;
         super.stats = stats;
     }
+
+    public void racePick(DataModel model, JTextPane t1, Hero hero) throws Exception{
+        o.output(model, t1, "Since you're playing on a computer, choose a RACE by\nentering \"1\" (Human), \"2\" (Orch), \"3\" (Elf) or \"4\" (Dwarf):", 0.0, true, false);
+    
+        inputPanel.setText("");
+        model.setInput("");
+        input = model.getInput();
+                 
+        while(model.getInput().equals(""))
+            Thread.sleep(100);
+                
+        if(model.getInput().equals("1"))
+            hero.setRace("Human");
+        else if(model.getInput().equals("2"))
+            hero.setRace("Orch");
+        else if(model.getInput().equals("3"))
+            hero.setRace("Elf");
+        else if(model.getInput().equals("4"))
+            hero.setRace("Dwarf");
+    }
+
+    public void genderPick(DataModel model, JTextPane t1, Hero hero) throws Exception{
+        o.output(model, t1, "Please enter (\"1\") for male,(\"2\") for female or (\"3\") for transgender.", 0.0, true);
+        
+        inputPanel.setText("");
+        model.setInput("");
+        input = model.getInput();
+                 
+        while(model.getInput().equals(""))
+            Thread.sleep(100);
+                
+        if(model.getInput().equals("1"))
+            hero.setGender("Male");
+        else if(model.getInput().equals("2"))
+            hero.setGender("Female");
+        else
+            hero.setGender("Transgender");
+    }
+
+    public void namePick(DataModel model, JTextPane t1, Hero hero) throws Exception{
+        inputPanel.setText("");
+        model.setInput("");
+        input = model.getInput();
+                    
+        while(model.getInput().equals(""))
+            Thread.sleep(100);
+                                                        
+        hero.setName(model.getInput());
+    }
+
 }
